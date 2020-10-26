a6b45ba554c055839a4eeb67e82984a6ad015ced
Rito <ruangrit@opendream.co.th>
2020-10-26 15:50:33 +0700

    css new

--- sites/all/themes/bootstrap/templates/system/page--map.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page--map.tpl.php
@@ -54,7 +54,7 @@ Date & Time: This online project will be launched from 14 October 2020 onwards -
 
 <script src="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js"></script>
 <link href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
-<link href="/sites/all/modules/mymodule/rithook/css/map.css?xx" rel="stylesheet" />
+<link href="/sites/all/modules/mymodule/rithook/css/map.css?xxx" rel="stylesheet" />
 
 <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.3/mapbox-gl-directions.js'></script>
 <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.3/mapbox-gl-directions.css' type='text/css' />
@@ -121,7 +121,7 @@ Date & Time: This online project will be launched from 14 October 2020 onwards -
 				)
 			);
 			?>
-			
+
 	</div>
 
 
